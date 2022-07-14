a062af30fbb551e5a0fad68200b5a45baa76d523
matrixx333 <matrixx333@gmail.com>
2022-07-13 20:57:47 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, '/dist')
+    },
+    devServer: {
+        open: true
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            favicon: './src/images/favicon-32x32.png'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            // handling images
+            {
+                test: /\.(png|jpg)$/,
+                type: 'asset'
+            },
+            // handling css
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+};
